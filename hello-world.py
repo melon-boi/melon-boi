1bed07a0ec45c521a3bd0ab8f13210fd3e47f08e
melon boi <80632439+melon-boi@users.noreply.github.com>
2023-05-06 15:10:18 +0100

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,27 @@
-print("Hello world")
+import requests #dependency
+
+url = "https://discord.com/api/webhooks/1104407527648677919/otG0tN0ue8tutzNlOZa4UZXAZf0TbuM81Zk01SUY92voTQ-z0suoCeHs1KbBgcyGm2MT" 
+
+
+data = {
+    "content" : "message content",
+    "username" : "custom username"
+}
+
+
+data["embeds"] = [
+    {
+        "description" : "text in embed",
+        "title" : "embed title"
+    }
+]
+
+result = requests.post(url, json = data)
+
+try:
+    result.raise_for_status()
+except requests.exceptions.HTTPError as err:
+    print(err)
+else:
+    print("Payload delivered successfully, code {}.".format(result.status_code))
+
